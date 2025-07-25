d1786c4704291a78e2b290a2076ea02860436145
Alexander Shenshin <alexander.shenshin@dsr-corporation.com>
2025-07-25 16:12:29 +0300

    Fix build and lint warnings

--- packages/signer-hashicorp-vault/tests/vault-signer-factory.spec.ts
+++ packages/signer-hashicorp-vault/tests/vault-signer-factory.spec.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/unbound-method */
 import { VaultSignerFactory } from '../src';
 import { VaultApi } from '../src/vault-api';
 
--- packages/verifier-hashicorp-vault/tests/vault-verifier-factory.spec.ts
+++ packages/verifier-hashicorp-vault/tests/vault-verifier-factory.spec.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/unbound-method */
 import { VaultVerifierFactory } from '../src';
 import { VaultApi } from '../src/vault-api';
 
